fa4a145d29467c915e37ca1cf8a56a883721fef0
Dan Danaila <dan.danaila@lacework.net>
2023-04-10 09:14:49 -0700

    Clean the install script

--- install.sh
+++ install.sh
@@ -1,54 +1,79 @@
 #!/bin/bash
 
+install_cmd() {
+  name="$1"
+  cmd="$2"
+  echo "---------------------------------------"
+  echo "[INSTALL][START] $name"
+  if ! command -v "$name" &> /dev/null; then
+    $cmd
+  else
+    echo "[INSTALL][STATUS] $name is already installed"
+  fi
+  echo "[INSTALL][END] $name"
+}
+
 ./set_dotfiles.sh
 
-# Install Plug, a plugin manager for VIM.
+echo "Install Plug, a plugin manager for VIM."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# Update the package sources list with the latest versions of the packages in
-# the repositories.
+
+echo "Update the package sources list with the latest versions of the packages"\
+  " in the repositories."
 sudo apt update
+
 # Integrated in VIM via ALE as a bash linter.
 # Extra: This is an open source static analysis tool that automatically finds
 # bugs in your shell scripts.
-sudo apt install shellcheck
+install_cmd "shellcheck" "sudo apt install shellcheck"
+
 # Integrated in VIM via ALE as bash formatter.
 # Extra: A shell parser, formatter, and interpreter. Supports POSIX Shell,
 # Bash, and mksh.
-go install mvdan.cc/sh/v3/cmd/shfmt@latest
-echo "Install tabname plugin."
-mkdir ~/.vim/plugin/
-wget -O ~/.vim/plugin/tabname.vim https://www.vim.org/scripts/download_script.php?src_id=6284
-echo "Install Java code formatter."
-brew install google-java-format
-echo "Installing Java19 as it is dependency for eclipselsp."
-DIR=~/lwcode/java
-mkdir "$DIR"
-(cd "$DIR" && \
-  wget https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.deb)
-yes | sudo apt install "$DIR"/jdk-19_linux-x64_bin.deb
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk-19/bin/java 1
-sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk-19/bin/javac 1
-sudo update-alternatives --install /usr/bin/jar jar /usr/lib/jvm/jdk-19/bin/jar 1
-echo 2 | sudo update-alternatives --config java
-echo 2 | sudo update-alternatives --config javac
-echo 2 | sudo update-alternatives --config jar
-export JAVA_HOME=/usr/lib/jvm/jdk-19
-echo "Install eclipselsp for Java linting."
-DIR=~/lwcode/eclipselsp
-if [ -d "$DIR" ];
-then
-  echo "Eclipselsp is already installed."
+install_cmd "shfmt" "go install mvdan.cc/sh/v3/cmd/shfmt@latest"
+
+echo "---------------------------------------"
+echo "[INSTALL][START] tabname plugin."
+tabname_file="$HOME/.vim/plugin/tabname.vim"
+if [[ -f "$tabname_file" ]]; then
+  echo "Tabname plugin already installed!"
 else
-  echo "Cloning repo..."
-  git clone https://github.com/eclipse/eclipse.jdt.ls.git "$DIR"
-  echo "Start the installation..."
-  (cd "$DIR" && ./mvnw clean verify -DskipTests=true)
+  mkdir ~/.vim/plugin/
+  wget -O ~/.vim/plugin/tabname.vim https://www.vim.org/scripts/download_script.php?src_id=6284
 fi
+
+install_cmd "jwt" "go install github.com/golang-jwt/jwt/cmd/jwt@latest"
+
+install_cmd "google-java-format" "brew install google-java-format"
+
+echo "---------------------------------------"
+echo "[INSTALL][START] jdk-19."
+if [[ -d "/usr/lib/jvm/jdk-19" ]]; then
+  echo "jdk-19 is already installed."
+else
+  DIR=~/lwcode/java
+  mkdir "$DIR"
+  (cd "$DIR" && \
+    wget https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.deb)
+  yes | sudo apt install "$DIR"/jdk-19_linux-x64_bin.deb
+  sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk-19/bin/java 1
+  sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk-19/bin/javac 1
+  sudo update-alternatives --install /usr/bin/jar jar /usr/lib/jvm/jdk-19/bin/jar 1
+  echo 2 | sudo update-alternatives --config java
+  echo 2 | sudo update-alternatives --config javac
+  echo 2 | sudo update-alternatives --config jar
+fi
+echo "[INSTALL][END] jdk-19."
+
+echo "---------------------------------------"
+echo "[INSTALL][START] Vim plugins."
 echo "Install the plugins for VIM via Plug."
 vim +PlugInstall +qall
 echo "Update the plugins for VIM via Plug."
 vim +PlugUpdate +qall
+echo "[INSTALL][END] Vim plugins."
+
 echo "Ensure that dans-playground is cloned."
 DIR=~/lwcode/dans-playground
 if [ -d "$DIR" ];
@@ -57,10 +82,10 @@ then
 else
   (cd ~/lwcode/ && git clone https://github.com/lacework-dev/dans-playground)
 fi
-echo "Fix timezone of machine."
+
+echo "---------------------------------------"
+echo "[INSTALL][START] Timezone."
 sudo apt-get install debconf-doc
 sudo ln -fs /usr/share/zoneinfo/US/Pacific /etc/localtime
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends tzdata
-echo "Install JWT cli."
-go get github.com/golang-jwt/jwt/cmd/jwt
-go install github.com/golang-jwt/jwt/cmd/jwt
+echo "[INSTALL][END] Timezone."
